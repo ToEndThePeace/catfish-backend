6a4cba9b02c473d9960a1f6b2d059b265aa83d53
John Afolabi <johnafolabi.da@gmail.com>
2020-04-02 20:50:01 +0100

    Write datatbase models

---
+++ models/instanceModel.js
@@ -0,0 +1,30 @@
+const db = require("../data/dbConfig");
+
+async function addInstance(instance) {
+  const newInstance = await db("data_instances")
+    .insert(instance)
+    .returning("*")
+    .then(data => data[0]);
+  return newInstance;
+}
+
+async function getInstancebyId(id) {
+  const instance = await db("data_instances")
+    .where("instance_id", id)
+    .select("*")
+    .first();
+  return instance;
+}
+
+async function getInstances() {
+  const instance = await db("data_instances")
+    .select("*")
+    .returning("*");
+  return instance;
+}
+
+module.exports = {
+  addInstance,
+  getInstancebyId,
+  getInstances
+};
