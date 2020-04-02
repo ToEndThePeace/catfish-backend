07a985ca41c6a796d5f7cb08fdaa0c1a6615d78b
ToEndThePeace <bran.ramirez.don@gmail.com>
2020-04-02 12:13:10 -0700

    Began writing out endpoints for deployment

---
+++ requests/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const router = express.Router();
+
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
+
+
+
+module.exports = router;
