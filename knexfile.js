3c04193370c0969916b2c84fd86f64f316a1d8dc
John Afolabi <johnafolabi.da@gmail.com>
2020-04-02 07:59:00 +0100

    Add new env for knex connection

--- knexfile.js
+++ knexfile.js
@@ -14,5 +14,17 @@ module.exports = {
       max: 10
     },
     useNullAsDefault: true
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    useNullAsDefault: true
   }
 };
