fac41dbe105e671bdbd5b9526b855add2a5e7f6d
John Afolabi <johnafolabi.da@gmail.com>
2020-04-03 14:46:39 +0100

    Re-structure files and routes

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
-require("dotenv").config();
+const server = require("./api/server");
 
 const port = process.env.PORT || 4000;
-const express = require("express");
-
-//Dev enpoint testing
-const testRoutes = require("./testing/testRoutes");
-const instanceRoutes = require("./routes/instances");
-const profileRoutes = require("./routes/profiles");
-const postRoutes = require("./routes/posts");
-
-const server = express();
-
-server.use(express.json());
-server.use("/test", testRoutes);
-server.use("/inst", instanceRoutes);
-server.use("/prof", profileRoutes);
-server.use("/feed", postRoutes);
-
-server.use("/", (req, res) => {
-  res.status(200).send("Hello World!!");
-});
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
